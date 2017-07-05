2eea1b0ec202d4190f543b03462c502b8b0af27e
lukeymac <lukey.mac@outlook.com>
2017-07-05 14:07:57 -0400

    add validations for image presence and type to Howls

--- app/models/howl.rb
+++ app/models/howl.rb
@@ -1,3 +1,6 @@
 class Howl < ActiveRecord::Base
-  has_attached_file :image
+  validates :image, presence: true
+
+  has_attached_file :image, styles { :medium => "640px" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
