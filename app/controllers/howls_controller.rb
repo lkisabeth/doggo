14bbb0f0994000682cb086a4c086dd4b2d677ae8
lukeymac <lukey.mac@outlook.com>
2017-07-06 11:57:52 -0400

    bugfix for #set_howl (typed set_post)

--- app/controllers/howls_controller.rb
+++ app/controllers/howls_controller.rb
@@ -1,5 +1,5 @@
 class HowlsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_howl, only: [:show, :edit, :update, :destroy]
 
   def index
     @howls = Howl.all
