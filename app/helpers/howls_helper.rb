a129ce31665f2593adc78da197ad0eef49590879
lukeymac <lukey.mac@outlook.com>
2017-07-07 23:38:56 -0400

    update howl forms with styling and js

--- app/helpers/howls_helper.rb
+++ app/helpers/howls_helper.rb
@@ -1,2 +1,6 @@
 module HowlsHelper
+  def form_image_select(howl)
+    return image_tag howl.image.url(:medium), id: 'image-preview', class: 'img-responsive' if howl.image.exists?
+    image_tag 'placeholder.jpg', id: 'image-preview', class: 'img-responsive'
+  end
 end
