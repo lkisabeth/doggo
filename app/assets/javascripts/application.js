e28abd9c9f28afc8d175d9a63b0989e6f0e183e1
lukeymac <lukey.mac@outlook.com>
2017-07-11 17:16:07 -0400

    organize styling and a few bug fixes... still some bugs with loading more comments...

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
-//= require jquery.turbolinks
 //= require jquery
-//= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
+//= require jquery.turbolinks
+//= require jquery_ujs
 //= require bootstrap-sprockets
 //= require bootstrap/dropdown
 //= require bootstrap/modal
