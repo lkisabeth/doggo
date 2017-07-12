8fd57f007b618a6866c31c5cff76fa19d526bdcb
lukeymac <lukey.mac@outlook.com>
2017-07-11 21:14:59 -0400

    building followers(trackers) functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170711180002) do
+ActiveRecord::Schema.define(version: 20170712003856) do
 
   create_table "barkbacks", force: :cascade do |t|
     t.integer "user_id"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20170711180002) do
     t.datetime "updated_at", null: false
     t.index ["howl_id"], name: "index_barkbacks_on_howl_id"
     t.index ["user_id"], name: "index_barkbacks_on_user_id"
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "following_id", null: false
+    t.integer "follower_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
+    t.index ["following_id", "follower_id"], name: "index_follows_on_following_id_and_follower_id", unique: true
+    t.index ["following_id"], name: "index_follows_on_following_id"
   end
 
   create_table "howls", force: :cascade do |t|
