5e4bccdddf6057cfbdfd3877146b965f23fe1523
lukeymac <lukey.mac@outlook.com>
2017-07-18 12:59:02 -0400

    bugfix for heroku deployment; removed callback url from devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,7 +251,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, '249679002200576', '17e912794ff5b9a33592dfca9a7f794b', callback_url: "http://localhost:3000/users/auth/facebook/callback"
+  config.omniauth :facebook, '249679002200576', '17e912794ff5b9a33592dfca9a7f794b'
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
