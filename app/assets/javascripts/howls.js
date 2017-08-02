9850c007c367070a516c99ae84509d7215b75b44
lukeymac <lukey.mac@outlook.com>
2017-08-01 20:50:13 -0400

    adding 'Get Json' button with jquery to Howls

--- app/assets/javascripts/howls.js
+++ app/assets/javascripts/howls.js
@@ -2,3 +2,35 @@ var loadFile = function(event) {
   var output = document.getElementById('image-preview');
   output.src = URL.createObjectURL(event.target.files[0]);
 };
+
+
+function Howl(attributes) {
+  this.id = attributes.id
+  this.image = attributes.image
+  this.caption = attributes.caption
+  this.user = attributes.user
+  this.barkbacks = attributes.barkbacks
+}
+
+Howl.succes = function(json) {
+  var howl = new Howl(json)
+  var howlDiv = user.renderDiv();
+  
+}
+
+
+$(document).ready(function(){
+  $("getJsonBtn").on('click', function(e) {
+    debugger
+    e.preventDefault()
+    var path = this.href;
+
+    $.ajax({
+      url: path,
+      dataType: 'json',
+      method: 'GET'
+    })
+    .success(Howl.success)
+    .error(Howl.error)
+  })
+})
